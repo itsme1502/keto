237bc5ec839a7f72860561a51a5505781ee80e08
vineetchauhan77 <vineetchauhan1003@gmail.com>
2024-01-22 03:47:47 +0530

    image

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { StyledEngineProvider } from '@mui/material'
+import HeaderImage from './components/HeaderImage'
 function App() {
 
   return (
     <>
       <StyledEngineProvider injectFirst>
 
-        <div className='bg-red-500'>sex</div>
+        <HeaderImage></HeaderImage>
       </StyledEngineProvider>
     </>
   )
