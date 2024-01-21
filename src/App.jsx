1aaa54bd2c992472adaf74e8031dfc418c7da8d9
Dark17 <abhishek171221@gmail.com>
2024-01-22 03:55:09 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { StyledEngineProvider } from '@mui/material'
+import ResponsiveAppBar from './components/Navbar/Navbar'
 function App() {
 
   return (
     <>
       <StyledEngineProvider injectFirst>
-
+        <ResponsiveAppBar/>
         <div className='bg-red-500'>sex</div>
       </StyledEngineProvider>
     </>
