426bcc42434aa23c69410bc6ec8599ba5c08a6a6
itsme1502 <harshitshankrit@gmail.com>
2024-01-22 01:20:06 +0530

    added tailwind fucntionality in mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import { StyledEngineProvider } from '@mui/material'
 function App() {
 
   return (
     <>
-      <div className='bg-red-500'>sex</div>
+      <StyledEngineProvider injectFirst>
+
+        <div className='bg-red-500'>sex</div>
+      </StyledEngineProvider>
     </>
   )
 }
